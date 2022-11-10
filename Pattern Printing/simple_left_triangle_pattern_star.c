0906e1f962158e6099f358c5872102ef29866c81
Shreeji Tiwari <87295390+shreejitiwari@users.noreply.github.com>
2022-11-10 19:27:54 +0530

    Add files via upload

---
+++ Pattern Printing/simple_left_triangle_pattern_star.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int row=1, col=1, n;
+    printf("Enter number of rows : ");
+    scanf("%d", &n);
+
+    while (row<=n)
+    {   
+        col = 1;
+        while (col<=row)
+        {
+            printf("*");
+            col++;
+        }
+        printf("\n");
+        row++;
+    }
+}
