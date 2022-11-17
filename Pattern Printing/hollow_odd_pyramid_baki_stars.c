c56027d7e5607eabb448b7a8850d5dfe54fa1728
Shreeji Tiwari <87295390+shreejitiwari@users.noreply.github.com>
2022-11-17 15:36:49 +0530

    Add files via upload

---
+++ Pattern Printing/hollow_odd_pyramid_baki_stars.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int row,col,n;
+    printf("Enter n : ");
+    scanf("%d",&n);
+
+    for (row=1; row<=n; row++)
+    {
+
+        for (col=1;col<=n-row+1;col++)
+        {
+            printf("*");
+        }
+   
+
+        for (col=1; col<=2*row-3; col++)
+        {
+            printf(" ",col);
+        }
+
+        if (row==1)
+        {
+            for (int i =0; i<n-row; i++)
+                printf("*");
+        }
+
+        else
+        {
+            for (col=1; col<=n-row+1;col++)
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+        
+ 
+    }
+    return 0;
+}
